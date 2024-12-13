ca8b215d4fdd5209317520200d23e1e9b569755d
alvarotoledo01 <toledojulian113@gmail.com>
2024-12-12 22:00:10 -0300

    ultimos cambios para prila

--- SistemaMedicoV5/Tests/obj/Debug/net6.0/Tests.AssemblyInfo.cs
+++ SistemaMedicoV5/Tests/obj/Debug/net6.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c20d77a30145fadc33148b51b1b7a8479693cf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ea83ce8c0f17629dd88879e0c1a5868b2de93ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
